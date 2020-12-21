8ce0dc1a4dee4e46862012073f0cf81cf49752d1
xKiraiChan <xKiraiChan@gmail.com>
2020-12-21 01:10:08 -0500

    initial commit

---
+++ BorderlessWindow/BorderlessWindow.cs
@@ -0,0 +1,16 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+[assembly: MelonGame()]
+[assembly: MelonInfo(typeof(BorderlessWindow.BorderlessWindow), "BorderlessWindow", "0", "xKiraiChan#8315", "github.com/xKiraiChan/BorderlessWindow")]
+
+namespace BorderlessWindow
+{
+    public class BorderlessWindow : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            Screen.SetResolution(int.MaxValue, int.MaxValue, true);
+        }
+    }
+}
